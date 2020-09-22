90ac52a5ab9f244a97c8e9a76dd3723255feaec9
Lucas-code-command <71079734+Lucas-code-command@users.noreply.github.com>
2020-09-22 16:33:18 -0300

    Add files via upload

---
+++ myApp.js
@@ -0,0 +1,9 @@
+var cellphone = {
+    model: "Apple" ,
+    year: "2019",
+    screen: 10,
+    buttons: ["on and off", "volum up", "volum down"],
+    buttonOn : function() {
+        return "on"
+    }
+};
